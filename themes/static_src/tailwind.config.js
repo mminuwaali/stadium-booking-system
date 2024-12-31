3ab4d5aae5ae2f29044b496fa27633bf5b95db09
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-12-31 12:46:28 +0100

    Update tailwind.config.js to include additional template paths and custom theme settings

--- themes/static_src/tailwind.config.js
+++ themes/static_src/tailwind.config.js
@@ -1,57 +1,46 @@
-/**
- * This is a minimal config.
- *
- * If you need the full config, get it from here:
- * https://unpkg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
- */
 
 module.exports = {
     content: [
-        /**
-         * HTML. Paths to Django template files that will contain Tailwind CSS classes.
-         */
-
-        /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
-        '../templates/**/*.html',
-
-        /*
-         * Main templates directory of the project (BASE_DIR/templates).
-         * Adjust the following line to match your project structure.
-         */
-        '../../templates/**/*.html',
-
-        /*
-         * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
-         * Adjust the following line to match your project structure.
-         */
-        '../../**/templates/**/*.html',
-
-        /**
-         * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
-         * patterns match your project structure.
-         */
-        /* JS 1: Ignore any JavaScript in node_modules folder. */
-        // '!../../**/node_modules',
-        /* JS 2: Process all JavaScript files in the project. */
-        // '../../**/*.js',
-
-        /**
-         * Python: If you use Tailwind CSS classes in Python, uncomment the following line
-         * and make sure the pattern below matches your project structure.
-         */
-        // '../../**/*.py'
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [
-        /**
-         * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
-         * for forms. If you don't like it or have own styling for forms,
-         * comment the line below to disable '@tailwindcss/forms'.
-         */
+        '../../**/templates/**/**/*.html',
+    ], plugins: [
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
         require('@tailwindcss/aspect-ratio'),
     ],
+    theme: {
+        extend: {
+            scale: {
+                '200': '2.0',
+                "250": "2.5",
+                '300': '3.0',
+                '350': '3.5',
+                '400': '4.0',
+                '450': '4.5',
+                '500': '5.0',
+            },
+            spacing: {
+                "1%": "1%",
+                "2%": "2%",
+                "3%": "3%",
+                "4%": "4%",
+                "5%": "5%",
+                "6%": "6%",
+                "7%": "7%",
+                "8%": "8%",
+                "9%": "9%",
+                "10%": "10%",
+                "15%": "15%",
+                "20%": "20%",
+                "25%": "25%",
+                "30%": "30%",
+                "35%": "35%",
+                "40%": "40%",
+                "45%": "45%",
+                "50%": "50%",
+            },
+            backgroundImage: {
+                "banner-horizontal": "url('/static/images/banner.jpg')",
+            }
+        },
+    },
 }
